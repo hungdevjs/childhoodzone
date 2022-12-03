66925cc2b00e62ed8c33fe57f3519cd407388f8b
jack <jack.nguyen@weareday.one>
2022-12-03 07:54:13 +0700

    init

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(8888, () => console.log('Server is running on port 8888'));
