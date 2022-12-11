2d7e10fec6bf01bbb08fa6e37ad2dc80b2903f5e
cleverlysisme <tuananhdao2862@gmail.com>
2022-12-11 22:17:32 +0700

    feat(#3): crud media

--- backend/routes/media.route.js
+++ backend/routes/media.route.js
@@ -1,5 +1,25 @@
 const express = require("express");
 
+const authMiddleware = require("../middlewares/auth.middleware");
+const mediaController = require("../controllers/media.controller");
+
 const mediaRoute = express.Router();
+
+mediaRoute.get("/media", authMiddleware.checkToken, mediaController.getMedia);
+mediaRoute.post(
+  "/media",
+  authMiddleware.checkToken,
+  mediaController.createMedia
+);
+mediaRoute.put(
+  "/media/:id",
+  authMiddleware.checkToken,
+  mediaController.updateMedia
+);
+mediaRoute.delete(
+  "/media/:id",
+  authMiddleware.checkToken,
+  mediaController.deleteMedia
+);
 
 module.exports = mediaRoute;
